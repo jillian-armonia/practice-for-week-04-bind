dc5634348caededcb59d62a73be4c5a9066b45dc
Jillian Armonia <jillian.marice@gmail.com>
2024-05-20 10:07:43 +0900

    created employee class and tested out bind

--- employee.js
+++ employee.js
@@ -1,1 +1,19 @@
 // Your code here
+class Employee {
+    constructor(name, occupation){
+        this.name = name;
+        this.occupation = occupation;
+        this.sayName = this.sayName.bind(this);
+        this.sayOccupation = this.sayOccupation.bind(this)
+    }
+
+    sayName(){
+        console.log(`${this.name} says hello`)
+    }
+
+    sayOccupation(){
+        console.log(`${this.name} is a ${this.occupation}`)
+    }
+}
+
+module.exports = Employee
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 // Your code here
+
+const Employee = require('./employee')
+
+const wick = new Employee("John Wick", "Dog Lover");
+
+setTimeout(wick.sayName, 2000) //It prints "John Wick says hello"
+setTimeout(wick.sayOccupation, 3000)//It prints "John Wick is a Dog Lover"
